9f1370973939d3aa196244b25c16b83022d4f4a6
Din Sopheak Panha <dinsopheakpanha24@gmail.com>
2026-02-02 02:06:20 -0500

    Added SLAM

---
+++ src/food_del_robot_description/launch/slam_online_mapping.launch.py
@@ -0,0 +1,47 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # --- Launch configurations ---
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    slam_params_file = LaunchConfiguration('slam_params_file')
+
+    # --- Declare arguments ---
+    declare_use_sim_time_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',  
+        description='Use simulation/Gazebo clock'
+    )
+
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value='/home/panha/Food_Delivery_Robot/src/food_del_robot/config/slam_online_mapping.yaml',
+        description='Full path to slam_toolbox YAML config'
+    )
+
+    # --- SLAM Toolbox (online async) ---
+    slam_toolbox_node = Node(
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen',
+        parameters=[
+            slam_params_file,
+            {'use_sim_time': use_sim_time}
+        ]
+    )
+
+    # --- Launch description ---
+    ld = LaunchDescription()
+
+    ld.add_action(declare_use_sim_time_argument)
+    ld.add_action(declare_slam_params_file_cmd)
+    ld.add_action(slam_toolbox_node)
+
+    return ld
