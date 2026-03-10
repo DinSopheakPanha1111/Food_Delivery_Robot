2486328cc8a4acf17b26520925b8cdfda3394d5a
TIKEAT <tikeat.te@gmail.com>
2026-03-10 17:14:29 +0700

    added a play with joystick no nav launch file

---
+++ src/food_del_robot_description/launch/play_drive.launch.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    joy_params = os.path.join(get_package_share_directory('food_del_robot_description'), 'config', 'joystick.yaml')
+
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        parameters=[joy_params],
+    )
+
+    teleop_node = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_node',
+        parameters=[joy_params],
+    )
+
+    drive_node = Node(
+        package='zlac8015d_driver',
+        executable='drive_and_odom',
+        name='drive_and_odom',
+    )
+
+    return LaunchDescription([
+        joy_node,
+        teleop_node,
+        drive_node,
+    ])
